a9a841f3442b0f2f5f9cb3c33cc3258e167621c7
Muthupandi <muthupandij26@gmail.com>
2024-01-09 22:59:55 +0530

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button className="px-3 py-1 m-2 bg-gray-200 rounded-md">All</button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/components/Head.js
@@ -0,0 +1,45 @@
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+
+const Head = () => {
+  const dispatch = useDispatch();
+
+  const handleToggleMenu = () => {
+    dispatch(toggleMenu());
+  };
+
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1 ">
+        <img
+          className="h-8 cursor-pointer"
+          src="https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-4.png"
+          alt="hamburgerMenu"
+          onClick={handleToggleMenu}
+        />
+        <img
+          className="h-8 mx-2"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+          alt="youTube Image"
+        />
+      </div>
+      <div className="col-span-10 text-center">
+        <input
+          className="w-1/2  border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
+        <button className="border border-gray-400 p-2 rounded-r-full">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
