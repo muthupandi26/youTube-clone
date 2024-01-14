0aabc1eed16eae9143062a6bcc5dd4fef8187b73
Muthupandi <muthupandij26@gmail.com>
2024-01-14 23:07:26 +0530

    implemeted search with cached data for optimizing the app

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,8 +1,69 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { useEffect, useState } from "react";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { cacheResults } from "../utils/searchSlice";
 
 const Head = () => {
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  console.log(searchQuery);
+
+  const searchCache = useSelector((store) => store.search);
+
+  useEffect(() => {
+    // make an api call after every key press
+    // but if the difference between 2 API calls is < 200ms
+    // decline the API call
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  {
+    /**
+     * key -i
+     * - render the component
+     * - useEffect();
+     * start timer => make api call after 200ms
+     *
+     * key - ip
+     * destroy the component (useEffect return method)
+     * re-render the component
+     * useEffect()
+     * - start  timer => make api call after 200ms
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+  }
+
+  const getSearchSuggestions = async () => {
+    console.log("API call", searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
 
   const handleToggleMenu = () => {
     dispatch(toggleMenu());
@@ -23,14 +84,31 @@ const Head = () => {
           alt="youTube Image"
         />
       </div>
-      <div className="col-span-10 text-center">
-        <input
-          className="w-1/2  border border-gray-400 p-2 rounded-l-full"
-          type="text"
-        />
-        <button className="border border-gray-400 p-2 rounded-r-full">
-          🔍
-        </button>
+      <div className="col-span-10 ">
+        <div>
+          <input
+            className="w-1/2  border border-gray-400 p-2 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 p-2 rounded-r-full">
+            🔍
+          </button>
+        </div>
+        <div className="fixed bg-white py-2 px-2 w-[29rem] shadow-lg rounded-lg">
+          {showSuggestions && (
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="shadow-sm py-2 hover:bg-gray-100 ">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
       <div className="col-span-1">
         <img
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,10 +1,12 @@
 import { configure } from "@testing-library/react";
 import appSlice from "./appSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import searchSlice from "./searchSlice";
 
 const store = configureStore({
   reducer: {
     app: appSlice,
+    search: searchSlice,
   },
 });
 
